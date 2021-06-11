b56a58a54f01b75b96e0e7c4c7002e1fe55f34e9
babylVoob <suphakin.th@gmail.com>
2021-06-12 04:05:41 +0700

    update

--- zero_in_fac.py
+++ zero_in_fac.py
@@ -3,4 +3,15 @@ def factorial(n):
         return 1
     return n * factorial(n - 1)
 
-factorial(5)
+
+def find_zero(result_num, c_zero):
+    while result_num[-1] == '0':
+        c_zero += 1
+        result_num = result_num[:len(result_num)-1]
+    return c_zero
+
+
+num = int(input('ENTER NUMBER YOU WANT TO FIND ZERO : ').strip())
+result = factorial(num)
+count_zero = find_zero(str(result), 0)
+print('%s ! = %s Have \'0\' last extend %s digit' % (num, result, count_zero))
